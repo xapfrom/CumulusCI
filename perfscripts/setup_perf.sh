76038dc54dd1f63901033f2aef898bb16d2c630e
Paul Prescod <pprescod@salesforce.com>
2020-03-07 06:18:20 -0800

    Results

--- perfscripts/setup_perf.sh
+++ perfscripts/setup_perf.sh
@@ -2,7 +2,7 @@ git clone https://github.com/SFDO-Tooling/CumulusCI.git
 cd CumulusCI
 git checkout feature/performance-tests
 export PYTHONPATH=.
-pip install -r requirements_dev.txt
-pip install keychain.alt
+pip install -r requirements.txt
+pip install keyrings.alt
 alias cci="python -m cumulusci"
 cci org connect abacus-perf-playground --default &
