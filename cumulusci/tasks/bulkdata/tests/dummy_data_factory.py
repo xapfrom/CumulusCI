2f9e7e262578946181e2a0ac1f6ff1a00d7432f2
Paul Prescod <pprescod@salesforce.com>
2020-03-08 09:38:33 -0700

    Simplify dummy data factory

--- cumulusci/tasks/bulkdata/tests/dummy_data_factory.py
+++ cumulusci/tasks/bulkdata/tests/dummy_data_factory.py
@@ -7,15 +7,7 @@ class GenerateDummyData(ModuleDataFactory):
     """Generate data based on test mapping.yml"""
 
     def make_records(self, num_records, factories, current_batch_num):
-        assert num_records % 4 == 0, "Use a batch size divisible by 4"
-        print("A")
-        self.logger.warning("A")
-        factories.create_batch("ContactFactory", num_records // 2, logger=self.logger)
-        self.logger.warning("B")
-        factories["ContactFactory"].create_batch(num_records // 4)
-        print("C")
-        self.logger.warning("B")
-        factories.create_batch("AccountFactory", num_records // 4, logger=self.logger)
+        factories.create_batch("ContactFactory", num_records, logger=self.logger)
 
 
 class AccountFactory(factory.alchemy.SQLAlchemyModelFactory):
