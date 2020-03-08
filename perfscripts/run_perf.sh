11d761f0d4fd908b3bf86073ffea892c2c45ac35
Paul Prescod <pprescod@salesforce.com>
2020-03-07 17:25:11 -0800

    Optimize

--- perfscripts/run_perf.sh
+++ perfscripts/run_perf.sh
@@ -1,6 +1,4 @@
+rm /tmp/data.db
 cci task run delete_data -o objects Account,Contact
-cci task run delete_data -o objects Contact,Account
-cci task run delete_data -o objects Account,Contact
-cci task run delete_data -o objects Contact,Account
 cci task run robot -o suites cumulusci/robotframework/perftests/long/ | tee /tmp/log.txt
 
