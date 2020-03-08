ecd7440387419aa2ad571becd41756fafef994bb
Paul Prescod <pprescod@salesforce.com>
2020-03-07 16:44:21 -0800

    Check out correct branch

--- perfscripts/setup_perf.sh
+++ perfscripts/setup_perf.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/SFDO-Tooling/CumulusCI.git
 cd CumulusCI
-git checkout feature/performance-tests
+git checkout feature/refactor_bulk_api__performance_test
 export PYTHONPATH=.
 pip install -r requirements.txt
 pip install keyrings.alt
